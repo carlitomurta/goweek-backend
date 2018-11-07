281461ef312828a22c2c962d88d2b7d5706ebdd9
Carlito Murta (Hanzo) <carlitomurtaramalho@gmail.com>
2018-11-06 23:16:26 -0200

    termino da aula 2 da goweek, implementando o socket.io(websocket)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 mongoose.connect(
   'mongodb://goweek:goweek123@ds217360.mlab.com:17360/goweekbackend',
   { useNewUrlParser: true }
 );
 
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
 app.use(require('./routes'));
 
 app.listen(3000, () => {
--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,11 @@ const express = require('express');
 const routes = express.Router();
 
 const TweetController = require('./controllers/TweetController');
+const LikeController = require('./controllers/LikeController');
 
-routes.get('tweets', TweetController.index);
-routes.post('tweet', TweetController.store);
+routes.get('/tweets', TweetController.index);
+routes.post('/tweet', TweetController.store);
+
+routes.post('/like/:id', LikeController.store);
 
 module.exports = routes;
